ac07961d320e856002c6d0b9bdfc3b467b3f7836
Jacob Lever <jacoblever@users.noreply.github.com>
2021-10-16 20:23:03 +0100

    Allow existing table to be modified for future events but not mast events

--- find-rsvps.js
+++ find-rsvps.js
@@ -5,24 +5,36 @@ const dateFormat = require('dateformat');
 
 const meetupUrlName = "Silicon-Throwabout";
 const eventName = "Indoor Ultimate Frisbee in *Hackney Community College* on Thursday evenings";
-const eventsAfter = "2021-09-01";
+const eventsDateRange = {from: "2021-09-01T00:00:00.000", to: "2022-01-01T00:00:00.000"};
 
 function getEvents(axios, meetupUrlName, eventName) {
   return axios({
     method: 'get',
-    url: `https://api.meetup.com/${meetupUrlName}/events/`,
+    url: `https://api.meetup.com/${meetupUrlName}/events?no_earlier_than=${eventsDateRange.from}&no_later_than=${eventsDateRange.to}&status=past,upcoming`,
     headers: { 'Accept': 'application/json' },
   }).then(response => {
     return response.data.filter(e => {
-      return e.name === eventName && dateFormat(e.local_date, "yyyy-mm-dd") > eventsAfter;
+      return e.name === eventName;
     });
   }).catch(err => {
     console.log(err);
-  })
+  });
 }
 
 function printLine() {
   console.log(`\n------------------------------------------------------------------------------`);
+}
+
+function updateEventAttendees(eventInfo) {
+  if (process.argv[3] && eventDataFormatted(eventInfo) == process.argv[3]) {
+    return true;
+  }
+  return eventInfo.eventData.status === "upcoming";
+}
+
+function eventDataFormatted(eventInfo) {
+  let date = eventInfo.eventData.local_date;
+  return dateFormat(date, "d mmm");
 }
 
 function getAttendees(axios, meetupUrlName, eventId) {
@@ -66,20 +78,16 @@ getEvents(axios, meetupUrlName, eventName).then(events => {
   console.log(`Session info by event`);
   Object.keys(infoByEvent).forEach(function (key) {
     let info = infoByEvent[key];
-    let date = info.eventData.local_date;
     let attendeeCount = info.attendeeNames.length;
     let attendees = info.attendeeNames.join(", ");
-    console.log(`${dateFormat(date, "d mmm")}: ${18 - attendeeCount} spaces left - (${attendees})`);
+    console.log(`${eventDataFormatted(info)}: ${18 - attendeeCount} spaces left - (${attendees})`);
   });
 
   printLine();
   console.log(`Session links by event`);
   Object.keys(infoByEvent).forEach(function (key) {
     let info = infoByEvent[key];
-    let date = info.eventData.local_date;
-    let attendeeCount = info.attendeeNames.length;
-    let attendees = info.attendeeNames.join(", ");
-    console.log(`- ${dateFormat(date, "d mmm")}: ${info.eventData.link}`);
+    console.log(`- ${eventDataFormatted(info)}: ${info.eventData.link}`);
   });
 
   printLine();
@@ -90,7 +98,7 @@ getEvents(axios, meetupUrlName, eventName).then(events => {
   });
   attendees.forEach(function (attendee) {
     let sessions = sessionsByPerson[attendee];
-    console.log(`${attendee}: ${sessions.length}`);
+    console.log(`${attendee}: ${sessions.length} (£${sessions.length * 5})`);
   });
 
   let people = Object.keys(sessionsByPerson).sort();
@@ -99,22 +107,49 @@ getEvents(axios, meetupUrlName, eventName).then(events => {
 
   let linkRow = ["https://tinyurl.com/SiThrowabout"];
   let dateRow = [""];
-  let costRow = [""];
   Object.keys(infoByEvent).forEach(key => {
-    let date = infoByEvent[key].eventData.local_date;
     linkRow.push(infoByEvent[key].eventData.link);
-    dateRow.push(dateFormat(date, "d mmm"));
-    costRow.push(5);
+    dateRow.push(eventDataFormatted(infoByEvent[key]));
   });
   matrixRows.push(linkRow);
   matrixRows.push(dateRow);
-  matrixRows.push(costRow);
 
-  people.forEach(person => {
+  let existingSheet = null;
+  let peopleAlreadyInSheet = [];
+  if (process.argv[2]) {
+    existingSheet = process.argv[2].split("\n").slice(2).map(row => row.split("\t"));
+    peopleAlreadyInSheet = existingSheet.map(row => row[0])
+  }
+
+  let peopleInMatrix = [];
+
+  peopleAlreadyInSheet.forEach((person, i) => {
+    let existingRow = existingSheet[i].slice(1); // remove name cell
+    let row = [person];
+    Object.keys(infoByEvent).forEach((key, j) => {
+      let eventInfo = infoByEvent[key];
+      let attending = infoByEvent[key].attendeeNames.some(x => x == person);
+      if (updateEventAttendees(eventInfo)) {
+        let wasGoing = existingRow[j] == 'y';
+        row.push(attending ? 'y' : (wasGoing ? 'd' : 'n'));
+      } else {
+        row.push(existingRow[j]);
+      }
+    });
+    matrixRows.push(row);
+    peopleInMatrix.push(person);
+  });
+
+  people.filter(x => !peopleAlreadyInSheet.includes(x)).forEach(person => {
     let row = [person];
     Object.keys(infoByEvent).forEach(key => {
-      let value = infoByEvent[key].attendeeNames.some(x => x == person) ? 'y' : 'n';
-      row.push(value);
+      let eventInfo = infoByEvent[key];
+      let attending = infoByEvent[key].attendeeNames.some(x => x == person);
+      if (updateEventAttendees(eventInfo)) {
+        row.push(attending ? 'y' : 'n');
+      } else {
+        row.push(attending ? 'y (dup)' : '');
+      }
     });
     matrixRows.push(row);
   });
@@ -122,6 +157,6 @@ getEvents(axios, meetupUrlName, eventName).then(events => {
   printLine();
   console.log(`Table for google sheet`);
   matrixRows.forEach(row => {
-    console.log(row.join(","))
+    console.log(row.join("\t"))
   });
 });
